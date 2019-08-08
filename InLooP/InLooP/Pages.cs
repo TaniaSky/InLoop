41f4cee743813dfa32e2e082107be65258a8b7e7
TaniaSky <promo.in.rivne@gmail.com>
2019-08-08 11:21:41 +0300

    Add files via upload

---
+++ InLooP/InLooP/Pages.cs
@@ -0,0 +1,118 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InLooP
+{
+    public class PageBase
+    {
+        public IWebDriver _driver;
+        public WebDriverWait _wait;
+
+        public PageBase(IWebDriver driver)
+        {
+            this._driver = driver;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(60));
+
+            WaitForPageLoad();
+            CloseCookieBanner();
+            PageFactory.InitElements(_driver, this);
+
+        }
+        
+        public void WaitForPageLoad()
+        {
+            Console.WriteLine("Waiting");
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            wait.Until((wdriver) =>
+               (wdriver as IJavaScriptExecutor).ExecuteScript("return document.readyState").Equals("complete")
+
+            );
+            wait.Until(wdriver => Int32.Parse((wdriver as IJavaScriptExecutor).ExecuteScript("if (window.jQuery) {return jQuery.active;  } else { return -1;}").ToString()) <= 0);
+            
+            // here need to check that angular loaded
+            // I tried few solutions, but they didn't work good
+            // need to prepare good javascript 
+
+            (_driver as IJavaScriptExecutor).ExecuteScript("window.scrollTo(0,document.body.scrollHeight);");
+            (_driver as IJavaScriptExecutor).ExecuteScript("window.scrollTo(0,document.body.scrollHeight);");
+
+            Thread.Sleep(5000);
+
+          
+        }
+
+        public void CloseCookieBanner()
+        {
+            if (_driver.FindElements(By.XPath("*//div[@aria-label='cookieconsent']")).Count>0 && _driver.FindElement(By.XPath("*//div[@aria-label='cookieconsent']")).Displayed)
+            {
+                _driver.FindElement(By.XPath("*//a[text() = 'I accept']")).Click();
+                WaitForPageLoad();
+            }
+        }
+        public virtual bool IsDisplayed()
+        {
+            return false;
+        }
+    }
+
+    public class RecentNewsPage : PageBase
+    {
+        [FindsBy(How = How.XPath, Using = "*//div/ul[not (contains (@class, 'widget'))][contains (@class, 'tag-list')]/li[@class='ng-scope']")]
+        public IList<IWebElement> MainMenuItems { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "*//div[@id='newsletters-archive']/div/article/header/h3/a")]
+        public IList<IWebElement> NewslettersItems { get; set; }
+
+        [FindsBy(How = How.TagName, Using = "body")]
+        public IWebElement Body { get; set; }
+
+        public void OpenNewsLetter(string label)
+        {
+            OpenQA.Selenium.Interactions.Actions actions = new OpenQA.Selenium.Interactions.Actions(_driver);
+            actions.MoveToElement(_driver.FindElement(By.XPath(String.Format("*//a[text()='{0}']", label)))).Build().Perform();
+            _driver.FindElement(By.XPath(String.Format("*//a[text()='{0}']", label))).Click();
+        }
+
+        public RecentNewsPage(IWebDriver driver) : base(driver) { }
+        public override bool IsDisplayed()
+        {
+            return MainMenuItems.Count>0;
+        }
+    }
+
+    public class MondayNewsLetterPage : PageBase
+    {
+        [FindsBy(How = How.XPath, Using = "*//span[text()='TOP INDUSTRY NEWS']")]
+        public IWebElement TopIndustryNewsLabel { get; set; }
+
+        public void OpenTag (string label)
+        {
+            _driver.FindElement(By.XPath(String.Format("*//*//a[@class='mcnButton'][text()='{0}']", label))).Click();
+        }
+        public MondayNewsLetterPage(IWebDriver driver) : base(driver) { }
+        public override bool IsDisplayed()
+        {
+            return _wait.Until(drv => TopIndustryNewsLabel.Displayed);
+        }
+    }
+
+    public class SelectedTagPage : PageBase
+    {
+        [FindsBy(How = How.XPath, Using = "*//h3[contains(@class, 'title')]")]
+        public IWebElement Title { get; set; }
+
+       
+        public SelectedTagPage(IWebDriver driver) : base(driver) { }
+        public override bool IsDisplayed()
+        {
+            return _wait.Until(drv => Title.Displayed);
+        }
+    }
+}
---
+++ InLooP/InLooP/Test.cs
@@ -0,0 +1,259 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Support;
+using System.Collections;
+
+namespace InLooP
+{
+    //here list of browsers can be added
+    [TestFixture(typeof(ChromeDriver))]
+    public partial class SetupBase<TWebDriver>  where TWebDriver : IWebDriver, new()
+    {
+        public IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Init();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            if (this._driver != null)
+            {
+                _driver.Close();
+                _driver.Quit();
+            }
+        }
+        public void Init()
+        {
+            var type = typeof(TWebDriver);
+
+            if (type == typeof(ChromeDriver))
+                _driver = InitializeChromeDriver();
+            else
+                _driver = InitOtherDriver();
+
+            _driver.Manage().Window.Position = new Point(0, 0);
+            _driver.Manage().Window.Size = new Size(Screen.PrimaryScreen.WorkingArea.Width,
+               Screen.PrimaryScreen.WorkingArea.Height);
+        }
+
+        private ChromeDriver InitializeChromeDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+
+            options.AddArguments("chrome.switches", "--disable-extensions");
+            options.AddArguments("disable-infobars");
+            options.AddArguments("start-maximized");
+
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            var driver = new ChromeDriver(service, options, TimeSpan.FromSeconds(150));
+            
+
+            return driver;
+        }
+
+        private IWebDriver InitOtherDriver()
+        {
+            //Just mock
+            return new ChromeDriver();
+        }
+
+        public void SwitchToLastTab()
+        {
+            List<String> browserTabs = new List<string>(_driver.WindowHandles);
+            _driver.SwitchTo().Window(browserTabs.Last());
+        }
+
+        public List<string> GetTopNewsletters()
+        {
+            var res = new List<string>();
+            var date = DateTime.Now;
+            if (date.Hour < 17)  //here need to know exact condition
+                date = date.AddDays(-1);
+
+            while (res.Count != 5)
+            {
+                if (date.DayOfWeek == DayOfWeek.Monday || date.DayOfWeek == DayOfWeek.Thursday)
+                    res.Add(String.Format("Newsletter {0}", date.ToString("d. MMMM. yyyy")));
+                date = date.AddDays(-1);
+            }
+
+            return res;
+        }
+
+        public string GetLastMondayNewsLetterTitle()
+        {
+            var res = String.Empty;
+            var date = DateTime.Now;
+            if (date.Hour < 17)  //here need to know exact condition
+                date = date.AddDays(-1);
+
+            while (res == String.Empty)
+            {
+                if (date.DayOfWeek == DayOfWeek.Monday)
+                    res = String.Format("Newsletter {0}", date.ToString("d. MMMM. yyyy"));
+                date = date.AddDays(-1);
+            }
+
+            return res;
+        }
+
+        public void SwitchToBaseTab()
+        {
+            List<String> browserTabs = new List<string>(_driver.WindowHandles);
+            while (browserTabs.Count > 1)
+            {
+                _driver.SwitchTo().Window(browserTabs.Last());
+                _driver.Close();
+                _driver.SwitchTo().Window(browserTabs[0]);
+                browserTabs = new List<string>(_driver.WindowHandles);
+            }
+           
+        }
+    }
+
+    [Parallelizable]
+    public class _001_GeneralTests_Newsletter<TWebDriver> : SetupBase<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        public string MainSiteUrl = "https://athletictrainers.inloop.com/en/news"; //Can be moved to config if have few test environments 
+        [SetUp]
+        public void OpenMainPage()
+        {
+            _driver.Navigate().GoToUrl(MainSiteUrl);
+            var _page = new RecentNewsPage(_driver); 
+            Assert.IsTrue(_page.IsDisplayed());
+        }
+
+        [Test]
+        public void _001_VerifyListIsCorrect()
+        {
+            Assert.Multiple(() =>
+            {
+                var _expecteNewsletterCount = 5;
+
+                var _page = new RecentNewsPage(_driver);
+                Assert.IsTrue(_page.IsDisplayed());
+
+                Assert.AreEqual(_expecteNewsletterCount, _page.NewslettersItems.Count);
+           
+                var _actualList = new List<string>();
+                foreach (var el in _page.NewslettersItems)
+                    _actualList.Add(el.Text);
+
+                var _expectedList = GetTopNewsletters();
+                CollectionAssert.AreEqual(_expectedList, _actualList);
+            });
+        }
+
+        [Test]
+        public void _002_VerifyLetterOpens()
+        {
+            var _page = new RecentNewsPage(_driver);
+            Assert.IsTrue(_page.IsDisplayed());
+
+            _page.OpenNewsLetter(GetLastMondayNewsLetterTitle());
+            SwitchToLastTab();
+
+            var _newsLetterPage = new MondayNewsLetterPage(_driver);
+            Assert.IsTrue(_newsLetterPage.IsDisplayed());
+        }
+
+        
+
+        
+    }
+
+    [Parallelizable]
+    public class _002_Monday_Newsletter<TWebDriver> : SetupBase<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        public string MainSiteUrl = "https://athletictrainers.inloop.com/en/news"; //Can be moved to config if have few test environments 
+        public string NewsLetterPageUrl = String.Empty;
+
+        [OneTimeSetUp]
+        public void GetNewsletterPage()
+        {
+            _driver.Navigate().GoToUrl(MainSiteUrl);
+            var _page = new RecentNewsPage(_driver);
+            Assert.IsTrue(_page.IsDisplayed());
+
+            _page.OpenNewsLetter(GetLastMondayNewsLetterTitle());
+            SwitchToLastTab();
+
+            var _newsLetterPage = new MondayNewsLetterPage(_driver);
+            Assert.IsTrue(_newsLetterPage.IsDisplayed());
+
+            NewsLetterPageUrl = _driver.Url;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver.Navigate().GoToUrl(NewsLetterPageUrl);
+            var _newsLetterPage = new MondayNewsLetterPage(_driver);
+            Assert.IsTrue(_newsLetterPage.IsDisplayed());
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            SwitchToBaseTab();
+        }
+
+        public static IList<string> TagsInLetter = new List<string>()
+        {
+            "Injuries",
+            "Football",
+            "Head Athletic Trainers",
+            "Head Injuries",
+            "NATA",
+            "Basketball"
+        };        
+
+        public static IEnumerable TagsInLetter_TestCaseSource
+        {
+            get
+            {
+                foreach (var el in TagsInLetter)
+                {
+                    yield return new TestCaseData(el)
+                        .SetName(String.Format("_003_TagInLetter_{0}",  el));
+                }
+            }
+        }
+
+
+        [Test, TestCaseSource(nameof(TagsInLetter_TestCaseSource))]
+        public void _001_VerifyTagOpensCorrectPage(String tag)
+        {
+            var _newsLetterPage = new MondayNewsLetterPage(_driver);
+            Assert.IsTrue(_newsLetterPage.IsDisplayed());
+
+            _newsLetterPage.OpenTag(tag);
+
+            SwitchToLastTab();
+
+            var _taggedPage = new SelectedTagPage(_driver);
+            Assert.IsTrue(_taggedPage.IsDisplayed());
+
+            Assert.AreEqual(tag.ToUpper(), _taggedPage.Title.Text.ToUpper());
+
+        }
+
+        
+
+
+
+    }
+}
